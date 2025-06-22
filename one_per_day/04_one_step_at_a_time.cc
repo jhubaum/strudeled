79f24dbe0411a5044c199ddc6887542b64db2ef5
Johannes Huwald <johannes.huwald@gmail.com>
2025-06-22 18:30:07 +0200

    Add track for today

---
+++ one_per_day/04_one_step_at_a_time.cc
@@ -0,0 +1,10 @@
+// One step at a time
+//
+// I almost didn't write this. The last week has been a lot, and I used today to rest.
+// I was tired and not in the mood to do anything. But in the evening, I pulled myself together
+// and sat down in front of my PC. This track is part of a pact I made with myself. And it doesn't
+// have to be good. It's just about moving forward. One step at a time...
+
+$: chord("<Cm7 Gm7 Fm7 Cm7>").voicing().sound("gm_pad_choir:5").room(".3")
+$: n("0 1 2 3").chord("<Cm7 Gm7 Fm7 Cm7>").voicing().s("gm_piano:4")
+$: n("<[0@2 1 [2 4] 3] [0*2]>").scale("C:minor").s("gm_electric_guitar_clean:4").gain("1")
