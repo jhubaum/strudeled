859adef1e50170026e9419b72bda08f96f80f4ca
Johannes Huwald <johannes.huwald@gmail.com>
2025-06-23 18:03:39 +0200

    Track for the day

---
+++ one_per_day/05_diminishing_returns.cc
@@ -0,0 +1,7 @@
+// Diminishing Returns
+//
+// I didn't write this track because I enjoyed doing it. I didn't try anything new
+// I did it to fulfil the streak.
+
+$: n("0 2*3 [1 2] <4 1>").scale("D:pentatonic").sound("gm_electric_guitar_muted")
+$: n("<0 [2, 4]*2 - 1 [3, 5]*2>*4").scale("D:pentatonic").sound("piano:2")
